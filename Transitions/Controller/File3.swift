e187d5128204a3c62399f1f75a91552ba9170718
smeth194243 <smor7432@740s-pl-114.local>
2016-10-03 12:01:46 -0600

    Linked the new view controller to the associated view controller in the storyboard

---
+++ Transitions/Controller/File3.swift
@@ -0,0 +1,9 @@
+//
+//  File3.swift
+//  Transitions
+//
+//  Created by Morris, Seth on 10/3/16.
+//  Copyright © 2016 Morris, Seth. All rights reserved.
+//
+
+import Foundation
---
+++ Transitions/Controller/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UpdatedViewController.swift
+//  Transitions
+//
+//  Created by Morris, Seth on 10/3/16.
+//  Copyright © 2016 Morris, Seth. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
